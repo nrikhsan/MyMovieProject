6421493981a9c1ba143027f37ce4540acd558226
ikhsan <nriksan11@gmail.com>
2023-08-05 22:55:18 +0700

    commit boss

--- app/src/main/java/com/jisoo/mymovie/adapter/AdapterAddMoviesToPersonalList.kt
+++ app/src/main/java/com/jisoo/mymovie/adapter/AdapterAddMoviesToPersonalList.kt
@@ -9,7 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.jisoo.mymovie.databinding.CardMovieBinding
+import com.jisoo.mymovie.databinding.CardMoviePersonalListBinding
 import com.jisoo.mymovie.response.ResultsItem
 import com.jisoo.mymovie.utils.Constants.URL_IMAGE_POSTER
 import com.jisoo.mymovie.utils.DateFormat
@@ -38,28 +38,27 @@ class AdapterAddMoviesToPersonalList: ListAdapter<ResultsItem, AdapterAddMoviesT
         }
     }
 
-    inner class ViewHolder(private val binding: CardMovieBinding): RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(private val binding: CardMoviePersonalListBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(item: ResultsItem) {
             binding.apply {
                 tvTitle.text = item.title
                 tvDate.text = DateFormat.formatDate(item.releaseDate, "dd MMMM yyyy")
-                tvOverview.text = item.overview
 
                 Glide.with(itemView.context)
                     .load(URL_IMAGE_POSTER + item.posterPath)
                     .diskCacheStrategy(DiskCacheStrategy.ALL)
                     .transform(RoundedCorners(20))
                     .into(imageList)
-            }
 
-            itemView.setOnClickListener {
-                addTo.addToPersonalList(item.id)
+                btnAddPersonalList.setOnClickListener {
+                    addTo.addToPersonalList(item.id)
+                }
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = CardMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = CardMoviePersonalListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
--- app/src/main/java/com/jisoo/mymovie/utils/EnumClassSortBy.kt
+++ app/src/main/java/com/jisoo/mymovie/utils/EnumClassSortBy.kt
@@ -4,8 +4,8 @@ enum class EnumClassSortBy(val value: String) {
 
     POPULARITY_DESCENDING("popularity.desc"),
     POPULARITY_ASCENDING("popularity.asc"),
-//    RELEASE_DATE_DESCENDING("release_date.desc"),
-//    RELEASE_DATE_ASCENDING("release_date.asc"),
-//    VOTE_AVERAGE_DESCENDING("vote_average.desc"),
-//    VOTE_AVERAGE_ASCENDING("vote_average.asc")
+    RELEASE_DATE_DESCENDING("release_date.desc"),
+    RELEASE_DATE_ASCENDING("release_date.asc"),
+    VOTE_AVERAGE_DESCENDING("vote_average.desc"),
+    VOTE_AVERAGE_ASCENDING("vote_average.asc")
 }
